8938f228a50e2b72aaae442f9bde7add8bbb345d
CodeWithViresh <codewithviresh@gmail.com>
2024-04-26 20:26:03 +0530

    this is done by the terminal fo vscode

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
 alert("hello how are you!")
+alert("hello how are you!")
+alert("hello how are you!")
+alert("hello how are you!")
 console.log("hello how are you today is very nice day right?? by the way what is your name?")
